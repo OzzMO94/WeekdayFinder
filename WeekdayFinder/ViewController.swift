85662893d405ead3d32778954eac14f9b0068ab6
OzzMO94 <adinovaaa@yandex.ru>
2022-02-06 18:32:53 +0300

    Initial Commit

---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Максим Хабиров on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var mounthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+    }
+    
+}
+
